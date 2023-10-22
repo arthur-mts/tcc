b3e91cc6bbdc383879172e3ba498b3f0a7f56120
Arthur Soares <arthur.soares@ifood.com.br>
2023-10-22 19:52:09 -0300

    implementando scripts

--- database/scripts/schema.sql
+++ database/scripts/schema.sql
@@ -2,23 +2,25 @@ create database tcc;
 use tcc;
 create table partido
 (
-    id       bigint primary key,
-    nome     text not null,
+    sigla    varchar(15) primary key,
+    nome     text   not null,
     situacao text,
-    uri_logo text not null,
-    uri_site text
+    uri_logo text   not null,
+    uri_site text,
+    id       bigint not null
 );
 
 create table deputado
 (
     id                 bigint primary key,
-    nome               text   not null,
+    nome               text not null,
     nome_eleitoral     text,
-    situacao           text   not null,
-    condicao_eleitoral text   not null,
-    partido_slug       varchar(5),
-    partido_id         bigint not null,
-    foreign key partido_fk (partido_id) references partido (id)
+    situacao           text not null,
+    condicao_eleitoral text not null,
+    partido            varchar(15),
+    estado             varchar(5) not null,
+    uri_foto           text,
+    foreign key partido_fk (partido) references partido (sigla)
 );
 
 create table proposicoes
@@ -27,12 +29,12 @@ create table proposicoes
     tipo              varchar(5) not null,
     ano               int        not null,
     ementa            text       not null,
-    ementa_detalhada  text       ,
+    ementa_detalhada  text,
     uri_documento     text       not null, ## Campo uri_inteiro_teor do documento
     data_apresentacao date,
-    ultimo_relator_id bigint     ,
+    ultimo_relator_id bigint,
     foreign key id_ultimo_relator_fk (ultimo_relator_id) references deputado (id),
-    situacao          text        ## Na API é retornado o campo codSituacao. A partir desse codigo irei pegar o texto e colocar aqui
+    situacao          text                 ## Na API é retornado o campo codSituacao. A partir desse codigo irei pegar o texto e colocar aqui
 );
 
 ## A partir do campo keywords retornado na API preencher essa tabela
@@ -47,24 +49,33 @@ create table proposicoes_keywords
 ## O valor dessas tabelas sera preechido com os CSVs disponibilizados nessa página:
 ## https://dadosabertos.camara.leg.br/swagger/api.html#staticfile -> Tesauro da câmara dos deputados
 
-create table tesauro_categorias(
-    id bigint primary key,
+create table tesauro_categorias
+(
+    id    bigint primary key,
     valor text not null
 );
 
-create table tesauro_subcategorias(
-    id bigint primary key,
+create table tesauro_subcategorias
+(
+    id           bigint primary key,
     categoria_id bigint not null,
-    foreign key categoria_id_fk (categoria_id) references tesauro_categorias(id),
-    valor text not null
+    foreign key categoria_id_fk (categoria_id) references tesauro_categorias (id),
+    valor        text   not null
 );
 
-create table tesauro_termos(
-    id bigint primary key,
+create table tesauro_termos
+(
+    id              bigint primary key,
     subcategoria_id bigint not null,
-    foreign key subcategoria_id_fk (subcategoria_id) references tesauro_subcategorias(id),
-    valor text not null
+    foreign key subcategoria_id_fk (subcategoria_id) references tesauro_subcategorias (id),
+    valor           text   not null
 );
+
+alter table proposicoes
+    drop column uri_documento;
+alter table proposicoes
+    add column uri_documento text default null;
+
 
 GRANT ALL PRIVILEGES ON tcc.* TO 'docker'@'%';
 
